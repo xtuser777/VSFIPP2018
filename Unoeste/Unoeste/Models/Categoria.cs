641d2e8a2f6c0a592e465efb0ef2affe032826d7
Lucas <lucaoxt@gmail.com>
2018-09-04 22:20:06 -0300

    Fifth commit, add example class library

---
+++ Unoeste/Unoeste/Models/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unoeste.Models
+{
+    internal class Categoria
+    {
+        private int _id;
+        private string _nome;
+
+        public int Id { get => _id; set => _id = value; }
+        public string Nome { get => _nome; set => _nome = value; }
+    }
+}
