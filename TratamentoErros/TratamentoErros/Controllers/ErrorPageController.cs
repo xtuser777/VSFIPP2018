f70239a032d37fb3ccee888fd4bf2a9250d0393b
Lucas da Silva Oliveira <lucaoxt@hotmail.com>
2018-10-29 22:15:42 -0300

    TratamentoErros

---
+++ TratamentoErros/TratamentoErros/Controllers/ErrorPageController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TratamentoErros.Controllers
+{
+    public class ErrorPageController : Controller
+    {
+        // GET: ErrorPage
+        public ActionResult Padrao()
+        {
+            if (Request.QueryString["aspxerrorpath"] != null)
+                ViewBag.URLErro = Request.QueryString["aspxerrorpath"];
+            return View();
+        }
+
+        public ActionResult Ops(int? id)
+        {
+            if(id.HasValue)
+            {
+                switch (id)
+                {
+                    case 404:
+                        return View("Erro404");
+                        break;
+                    case 500:
+                        return View("Erro500");
+                        break;
+                    case 403:
+                        return View("Erro403");
+                        break;
+                    default:
+                        return View("Padrao");
+                        break;
+                }
+            }
+            else
+            {
+                return View("Padrao");
+            }
+        }
+    }
+}
---
+++ TratamentoErros/TratamentoErros/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TratamentoErros.MvcApplication" Language="C#" %>
